d7085508fb3b5b208a20185888cc0c4e4ed967b2
CristianMira <krhistian1902@gmail.com>
2023-05-01 19:49:25 -0500

    Form

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,32 @@
-import React from "react";
-
+"use client";
+import { useState } from "react";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [file, setFile] = useState(null);
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          if (!file) return;
+
+          console.log(file);
+        }}
+      >
+        <label>Upload file</label>
+
+        <input
+          type="file"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
+
+        <button>Upload</button>
+      </form>
+    </div>
+  );
 };
 
 export default HomePage;
