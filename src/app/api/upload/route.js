b516cd53354b2e111c03abd0996895222490b2ce
CristianMira <krhistian1902@gmail.com>
2023-05-01 22:39:24 -0500

    Chages in API

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,15 +1,17 @@
 import { writeFile } from "fs/promises";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
   const data = await request.formData();
   const file = data.get("file");
 
+  if (!file) {
+    return NextResponse.json({ success: false, message: "No file uploaded" });
+  }
   const bytes = await file.arrayBuffer();
   const buffer = Buffer.from(bytes);
 
   writeFile(`./public/${file.name}`, buffer);
 
-  return new Response(
-    JSON.stringify({ message: "File uploaded successfully" })
-  );
+  return NextResponse.json({ success: true, message: "File uploaded" });
 }
--- src/app/page.js
+++ src/app/page.js
@@ -3,33 +3,38 @@ import { useState } from "react";
 const HomePage = () => {
   const [file, setFile] = useState(null);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!file) return;
+
+    try {
+      const data = new FormData();
+      data.set("file", file);
+
+      const res = await fetch("/api/upload", {
+        method: "POST",
+        body: data,
+      });
+      console.log(res);
+
+      if (res.ok) {
+        console.log("File uploaded successfully");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
   return (
     <div>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-
-          if (!file) return;
-
-          const form = new FormData();
-          form.set("file", file);
-
-          const res = await fetch("/api/upload", {
-            method: "POST",
-            body: form,
-          });
-          const data = await res.json();
-          console.log(data);
-        }}
-      >
+      <form onSubmit={handleSubmit}>
         <label>Upload file</label>
 
-        <input
-          type="file"
-          onChange={(e) => {
-            setFile(e.target.files[0]);
-          }}
-        />
+        <input type="file" onChange={handleChange} />
 
         <button>Upload</button>
       </form>
