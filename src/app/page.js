bced2b8f9df0841835f50a53b48bf1111daf0170
CristianMira <krhistian1902@gmail.com>
2023-05-03 21:43:58 -0500

    Styles and preview image

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
-
 import { useState } from "react";
+import Image from "next/image";
+
 const HomePage = () => {
   const [file, setFile] = useState(null);
 
@@ -32,22 +33,32 @@ const HomePage = () => {
 
   return (
     <div className="flex h-screen justify-center items-center">
-      <form className="bg-zinc-950 p-5" onSubmit={handleSubmit}>
+      <div className="bg-zinc-950 p-5">
         <h1 className="text-4xl text-center my-4">Upload a file</h1>
+        <form className="bg-zinc-950 p-5" onSubmit={handleSubmit}>
+          <input
+            className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
+            type="file"
+            onChange={handleChange}
+          />
 
-        <input
-          className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
-          type="file"
-          onChange={handleChange}
-        />
+          <button
+            className="bg-green-400 text-zinc-100 p-2 rounded block w-full disabled:opacity-50"
+            disabled={!file}
+          >
+            Upload
+          </button>
+        </form>
 
-        <button
-          className="bg-green-400 text-zinc-100 p-2 rounded block w-full disabled:opacity-50"
-          disabled={!file}
-        >
-          Upload
-        </button>
-      </form>
+        {file && (
+          <Image
+            width={250}
+            height={250}
+            className="w-64 h-64 object-cover mx-auto"
+            src={URL.createObjectURL(file)}
+          />
+        )}
+      </div>
     </div>
   );
 };
