a29258d02ecbea1c938f3163509f5213c0416297
CristianMira <krhistian1902@gmail.com>
2023-05-03 21:44:59 -0500

    FIX

--- src/app/page.js
+++ src/app/page.js
@@ -35,7 +35,7 @@ const HomePage = () => {
     <div className="flex h-screen justify-center items-center">
       <div className="bg-zinc-950 p-5">
         <h1 className="text-4xl text-center my-4">Upload a file</h1>
-        <form className="bg-zinc-950 p-5" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
           <input
             className="bg-zinc-900 text-zinc-100 p-2 rounded block mb-2"
             type="file"
